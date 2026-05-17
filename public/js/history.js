58ed8181535184a568139086d03541babae9e35f
cocom33 <ancientcococ3@gmail.com>
2026-05-17 17:03:59 +0700

    update perbaikan

--- public/js/history.js
+++ public/js/history.js
@@ -15,6 +15,19 @@ document.addEventListener('DOMContentLoaded', async function () {
   }
 });
 
+function test() {
+  const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.set("X-Custom-Header", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2dlbmktY3Bucy5tYWhpcnRlY2huby5teS5pZC9hcGkvdjEvdmVyaWZ5LW90cCIsImlhdCI6MTc1Mzg0NTAwMywiZXhwIjoxMDM5Mzg0NTAwMywibmJmIjoxNzUzODQ1MDAzLCJqdGkiOiJXY1hwMXBpOGNjOUNLUnRzIiwic3ViIjoiOWY3OWJmMTYtMzFmNy00MjgzLWI5NmQtMzE3OGEwMjY3ZjMyIiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.2ymZAK9Vquv6DCw_f1hnYmWMMXQYpH-7RF53ZKi-hrs");
+    fetch("https://geni-cpns.mahirtechno.my.id/api/v1/admin/me", {
+      method: "GET",
+      headers: myHeaders,
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error("Error:", error));
+}
+
 function renderTable(data) {
   const tbody = document.getElementById('history-table-body');
   if (data == null) return
